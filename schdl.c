9539c22da07efc66a20b48e35ea6cd3b38c1fe68
nemote95 <nemote95@gmail.com>
2016-05-17 15:26:22 +0430

    pass array of cpus to schedual function

--- schdl.c
+++ schdl.c
@@ -8,25 +8,25 @@ int time;
 //proc struct
 struct proc {
 	int pid;
-	int burn;
+	int execution_time;
 	struct list_head list_member;
 };
 
 //cpu struct
 struct cpu {
 	int cid;
-	int q;
+	int quantum_time;
 	struct list_head queue;
 };
 
 //add proc node to a queue
-void add_node(int pid,int burn, struct list_head *head)
+void add_node(int pid,int execution_time, struct list_head *head)
 {
     struct proc *procPtr = (struct proc *)malloc(sizeof(struct proc));
     assert(procPtr != NULL);
     
     procPtr->pid = pid;
-    procPtr->burn = burn;
+    procPtr->execution_time = execution_time;
     INIT_LIST_HEAD(&procPtr->list_member);
     list_add_tail(&procPtr->list_member, head);
 }
@@ -39,7 +39,7 @@ void display(struct list_head *head)
     __list_for_each(iter, head) {
         objPtr = list_entry(iter, struct proc, list_member);
         printf("%d ", objPtr->pid);
-        printf("%d|", objPtr->burn);
+        printf("%d|", objPtr->execution_time);
     }
     printf("\n");
 }
@@ -51,8 +51,7 @@ void FIFO(struct list_head *head){
    	redo:
     __list_for_each(iter,head) {
         objPtr = list_entry(iter, struct proc, list_member);
-        time+=objPtr->burn;
-        printf("time %d \n",time);
+        time+=objPtr->execution_time;
 		list_del(&objPtr->list_member);
 		free(objPtr);
         goto redo;
@@ -60,53 +59,56 @@ void FIFO(struct list_head *head){
 }
 
 
-void schedual(struct list_head *head, struct list_head *head2,int q1){
+void schedual(struct cpu **array,int length){
+	int i;
+	for(i=0;i<length-1;i++){
+	struct list_head *head1=&array[i]->queue;
+	struct list_head *head2=&array[i+1]->queue;
 	struct list_head *iter;
     struct proc *objPtr;
    	redo:
-    __list_for_each(iter,head) {
+    __list_for_each(iter,head1) {
+		printf("time : %d \n",time);
 		objPtr = list_entry(iter, struct proc, list_member);
-		int q=q1;
-		while ( q && objPtr->burn){
+		int q=array[i]->quantum_time;
+		while ( q && objPtr->execution_time){
 		q--;
 		time++;
-		objPtr->burn--;}
-        printf("time %d \n",time);
+		objPtr->execution_time--;}
         int pid=objPtr->pid;
-        int burn=objPtr->burn;
+        int execution_time=objPtr->execution_time;
 		list_del(&objPtr->list_member);
 		free(objPtr);
-        if (burn>0 && head2){add_node(pid,burn,head2);}
-        goto redo;
+        if (execution_time>0 && head2){add_node(pid,execution_time,head2);}
+        goto redo;}
         
     }
+    FIFO(&array[length-1]->queue);
 }
 	
 	
 int main() 
 {
-	//creating cpus
+	//struct cpus
 	struct cpu *array[3];
-	int i=0;
-	for (i=0;i<3;i++){
+	int length=3;
+	int i;
+	for (i=0;i<length;i++){
 		struct cpu *cpuPtr1 = (struct cpu *)malloc(sizeof(struct cpu));
 		assert(cpuPtr1 != NULL);
 		INIT_LIST_HEAD(&cpuPtr1->queue);
 		cpuPtr1->cid=i;
+		cpuPtr1->quantum_time=(i+1)*4;
 		array[i]=cpuPtr1;
 		}
 	
-    //adding nodes to first cpu queue
+    //add process nodes to the first cpu queue
     add_node(1,10, &array[0]->queue);
     add_node(2,15, &array[0]->queue);
     add_node(3,20, &array[0]->queue);
     add_node(4,25, &array[0]->queue);
     
-    display(&array[0]->queue);
-    
-    for(i=0;i<2;i++){
-    schedual(&array[i]->queue, &array[i+1]->queue,4*(i+1) );}
-    FIFO(&array[2]->queue);
+    schedual(array,length);
     
     return 0;
 }
